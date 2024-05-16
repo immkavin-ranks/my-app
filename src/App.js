949ee4997bbdbf62e9d78b31a2320c8cf2fba8d9
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-05-16 22:15:32 +0530

    Use braces to pass an expression

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <MyComponent name="Kavin" />
+        <MyComponent name={ "Kavin" + 26 } />
       </header>
     </div>
   );
